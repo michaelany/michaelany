d2052fd727e23f8c3786cd96263544c666c39a63
m1shgun <michael@7glyphs.com>
2018-03-19 14:46:21 +0300

    continue experience

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -12,7 +12,7 @@ const responsibilities = [
         subItems: [
             {id: 1, text: 'Прием и ведение заявок в Service Desk'},
             {id: 2, text: <span><strong>Консультирование</strong> сотрудников</span>},
-            {id: 3, text: <span><strong>Решение проблем</strong>Р с программным обеспечением и железом</span>},
+            {id: 3, text: <span><strong>Решение проблем</strong> с программным обеспечением и железом</span>},
             {id: 4, text: <span><strong>Установка</strong> и <strong>диагностика</strong> ПО</span>},
             {id: 5, text: 'Диагностика работы серверов'}
         ]
@@ -29,8 +29,18 @@ const responsibilities = [
         subItems: [
             {id: 1, text: <span><strong>Разработка</strong> клиентской бизнес-логики и компонентов системы</span>},
             {id: 2, text: <span><strong>Совершенствование</strong> архитектуры приложения</span>},
+            {id: 3, text: <span><strong>Верстка</strong> страниц и компонентов интерфейса</span>},
+            {id: 4, text: 'Адаптивная и кроссбраузерная верстка'},
+        ]
+    },
+    {id: 4, text: <span>Работаю удаленно <strong>frontend-разработчиком</strong> в компании <Href href={LINKS.experience.sevenGlyphs}>"7 glyphs"</Href>:</span>,
+        subItems: [
+            {id: 1, text: <span><strong>Разработка</strong> клиентской бизнес-логики и компонентов систем</span>},
+            {id: 2, text: <span><strong>Проектирование</strong> архитектуры приложений</span>},
             {id: 3, text: <span><strong>Сопровождение</strong> кода</span>},
-            {id: 4, text: <span>Адаптивная и кроссбраузерная <strong>верстка</strong></span>},
+            {id: 4, text: <span><strong>Верстка</strong> страниц и компонентов интерфейса</span>},
+            {id: 5, text: 'Адаптивная и кроссбраузерная верстка'},
+            {id: 6, text: 'Учет затраченного времени на выполнение задач'},
         ]
     }
 ];
