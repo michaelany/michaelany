2048c04fe9e68df5dd9ae178edee5bf695d57c18
m1shgun <michael@7glyphs.com>
2018-03-28 00:28:15 +0300

    change animation

--- src/components/_common/Info/InfoExtra/index.js
+++ src/components/_common/Info/InfoExtra/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import cn from 'classnames';
+import Collapse from 'material-ui/transitions/Collapse';
 
 import './style.css';
 import ScrollAnimation from '../../../_common/ScrollAnimation';
@@ -8,7 +8,7 @@ import MuiButton from '../../../_common/MuiButton';
 
 export default class InfoExtra extends Component {
     state = {
-        contentHeight: 0
+        isExtraOpen: false
     };
 
     static propTypes = {
@@ -16,46 +16,26 @@ export default class InfoExtra extends Component {
         color: PropTypes.string.isRequired
     };
 
-    componentDidMount() {
-        window.addEventListener('resize', this.handleContentHeightSet);
-    }
+    handleExtraOpen = () => this.setState({
+        isExtraOpen: true
+    });
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleContentHeightSet);
-    }
-
-    handleContentHeightSet = () => {
-        const {contentHeight} = this.state;
-        const blockHeight = this.block.clientHeight;
-        if (contentHeight && contentHeight !== blockHeight) this.setState({
-            contentHeight: blockHeight + 20
-        });
-    };
-
-    handleContentHeightToggle = () => this.setState((prevState) => ({
-        contentHeight: prevState.contentHeight ? 0 : this.block.clientHeight + 20
-    }));
-
-    setBlockRef = (ref) => this.block = ref;
+    handleExtraClose = () => this.setState({
+        isExtraOpen: false
+    });
 
     render() {
         const {extra, color} = this.props;
-        const {contentHeight} = this.state;
-        const contentClass = cn('info-extra__content', {'info-extra__content_open': contentHeight});
-        const contentStyle = {
-            height: contentHeight
-        };
+        const {isExtraOpen} = this.state;
         const extraContent = extra.map(({id, text}) => <p key={id}>{text}</p>);
-        const buttonText = contentHeight ? 'Короче' : 'Подробнее';
+        const buttonText = isExtraOpen ? 'Короче' : 'Подробнее';
         return (
             <div className="info-extra">
-                <div className={contentClass} style={contentStyle}>
-                    <div className="info-extra__block" ref={this.setBlockRef}>
-                        {extraContent}
-                    </div>
-                </div>
+                <Collapse in={isExtraOpen} timeout={800}>
+                    {extraContent}
+                </Collapse>
                 <ScrollAnimation name="bounceInUp">
-                    <MuiButton variant="raised" kind={color} onClick={this.handleContentHeightToggle}>
+                    <MuiButton variant="raised" kind={color} onClick={isExtraOpen ? this.handleExtraClose : this.handleExtraOpen}>
                         {buttonText}
                     </MuiButton>
                 </ScrollAnimation>
