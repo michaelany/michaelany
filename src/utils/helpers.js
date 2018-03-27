077c4ce309c75265b251f10cf917c5390636b3e3
m1shgun <michael@7glyphs.com>
2018-03-27 12:11:42 +0300

    continue responsive

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,8 +3,7 @@ import cn from 'classnames';
 export const getPanelClass = (panel, pathname) => cn(`${panel} ${panel}_page_${pathname.split('/')[1] || 'home'}`, {
     [`${panel}_various`]: pathname === '/',
     [`${panel}_red`]: pathname === '/portfolio/lazy-az' || pathname === '/portfolio/my-portfolio',
-    [`${panel}_yellow`]: pathname === '/portfolio/lazy-az-dashboard'
-    || pathname === '/portfolio/12-saffron',
+    [`${panel}_yellow`]: pathname === '/portfolio/lazy-az-dashboard' || pathname === '/portfolio/12-saffron',
     [`${panel}_grass`]: pathname === '/portfolio/7-glyphs' || pathname === '/portfolio/service-packages',
     [`${panel}_green`]: pathname === '/about' || pathname === '/portfolio/whip-around',
     [`${panel}_blue`]: pathname === '/skills' || pathname === '/portfolio/msp',
