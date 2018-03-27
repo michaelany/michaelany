1daa62798f0b7a22c42c1e4f03ad2a7d097f75bf
m1shgun <michael@7glyphs.com>
2018-03-27 17:21:29 +0300

    continue responsive

--- src/components/Home/Greeting/index.js
+++ src/components/Home/Greeting/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withRouter} from 'react-router-dom';
+import Typist from 'react-typist';
 
 import './style.css';
 import MuiButton from '../../_common/MuiButton';
@@ -24,15 +25,26 @@ class Greeting extends Component {
     render() {
         return (
             <section className="greeting section section_first f fd-c jc-c">
-                <div className="greeting__hello title">Привет,</div>
-                <h1 className="greeting__title title">
-                    меня зовут<br />
-                    <span className="greeting__letter">М</span>
-                    ихаил <span className="greeting__letter">А</span>наньев,<br />
-                    я веб-разработчик
+                <Typist cursor={{show: false}}>
+                    <div className="greeting__title">Привет,</div>
+                </Typist>
+                <Typist cursor={{show: false}} startDelay={1000}>
+                    <h1 className="greeting__title greeting__title_name">меня зовут</h1>
+                </Typist>
+                <h1 className="greeting__title greeting__title_name">
+                    <span className="greeting__name greeting__name_first">
+                        <span className="greeting__letter">М</span>ихаил
+                    </span>
+                    &nbsp;
+                    <span className="greeting__name greeting__name_last">
+                        <span className="greeting__letter">А</span>наньев,
+                    </span>
                 </h1>
-                <h2 className="greeting__subtitle subtitle">Front-End Developer / React JS</h2>
-                <div>
+                <Typist cursor={{show: false}} startDelay={3200}>
+                    <h1 className="greeting__title greeting__title_name">я веб-разработчик</h1>
+                </Typist>
+                <h2 className="greeting__subtitle">Front-End Developer / React JS</h2>
+                <div className="greeting__buttons">
                     <MuiButton isMargin variant="raised" kind="blue" style={styles.button} onClick={this.handleGoToPortfolio}>
                         Портфолио
                     </MuiButton>
