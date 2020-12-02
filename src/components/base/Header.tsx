1aa4fe429736b81ee3f45f4f0c839f810bd1025a
Michael Ananiev <mclananiev@gmail.com>
2020-12-02 16:33:38 +0300

    add Header Slide

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Slide from '@material-ui/core/Slide'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/MenuRounded'
 
@@ -9,6 +10,7 @@ import Drawer from './Drawer'
 
 export default function Header(): JSX.Element {
   const [open, toggleOpen] = useState<boolean>(false)
+  const trigger: boolean = useScrollTrigger()
 
   const handleOpen = (): void => toggleOpen(true)
 
@@ -16,16 +18,18 @@ export default function Header(): JSX.Element {
 
   return (
     <>
-      <header className="Header">
-        <IconButton
-          className="Colorful"
-          aria-label="Показать"
-          onClick={handleOpen}
-        >
-          <MenuIcon className="ButtonIcon" />
-        </IconButton>
-        <Logo />
-      </header>
+      <Slide appear={false} in={!trigger}>
+        <header className="Header">
+          <IconButton
+            className="Colorful"
+            aria-label="Показать"
+            onClick={handleOpen}
+          >
+            <MenuIcon className="ButtonIcon" />
+          </IconButton>
+          <Logo />
+        </header>
+      </Slide>
       <Drawer open={open} onClose={handleClose} />
     </>
   )
