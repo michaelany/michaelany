f51929d36758ed532085817ff75549d3aa6ce11c
Michael Any <michael.any.dev@gmail.com>
2023-06-09 00:33:59 +1200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://michaelany.com/sw.js', { scope: 'https://michaelany.com/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
